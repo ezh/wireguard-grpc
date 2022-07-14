c0fe112ad6202c4ef67649f9bc8a989b08d05baa
Alexey Aksenov <ezh@ezh.msk.ru>
2022-07-14 16:31:28 +0400

    add versions file

---
+++ cmd/cmd_version.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Prints version",
+	Long:  "Prints the program’s version information.",
+	Args:  cobra.ExactArgs(0),
+	Run: func(cmd *cobra.Command, args []string) {
+		git := ""
+		r := strings.TrimSpace(Revision)
+
+		if len(r) > 0 {
+			git = fmt.Sprintf(" (r%s)", r)
+		}
+
+		fmt.Printf("%s %s%s", AppName, Version, git) //nolint:forbidigo
+	},
+}
